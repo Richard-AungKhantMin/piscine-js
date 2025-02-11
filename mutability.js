4687f20c680137721cf9c9217aab7561470efb24
amin <aungkhantmin.dev@gmail.com>
2025-02-11 10:59:03 +0200

    added ; and added the undef to US

--- mutability.js
+++ mutability.js
@@ -1,11 +1,16 @@
 const person = {
     age : 10,
-    country,
-}
+    country : "US",
+};
 
-const clone1 = Object.assign({}, person)
-const clone2 = {...person}
-const samePerson = person
+const clone1 = Object.assign({}, person);
+const clone2 = {...person};
+const samePerson = person;
 
-person.age += 1
-person.country = 'FR'
+person.age += 1;
+person.country = 'FR';
+
+console.log(clone1); // { age: 10, country: "US" } (unchanged)
+console.log(clone2); // { age: 10, country: "US" } (unchanged)
+console.log(samePerson); // { age: 11, country: "FR" } (same as person)
+console.log(person); // { age: 11, country: "FR" }
