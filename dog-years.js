7c1dbaa98bd39412076811f0d5fe9e99aca74d0d
amin <aungkhantmin.dev@gmail.com>
2025-02-11 12:19:18 +0200

    lee pl

--- dog-years.js
+++ dog-years.js
@@ -5,38 +5,40 @@ function dogYears(planet, ageInSec){
 
     switch(planet){
         case "earth":
-        ans = ratio * 1
+        ans = ratio / 1
             break
             
         case "mercury":
-        ans = ratio * 0.2408467
+        ans = ratio / 0.2408467
             break
             
         case "venus":
-        ans = ratio * 0.61519726
+        ans = ratio / 0.61519726
             break
         
         case "mars":
-        ans = ratio * 1.8808158
+        ans = ratio / 1.8808158
             break
             
         case "jupiter":
-        ans = ratio * 11.862615
+        ans = ratio / 11.862615
             break
             
         case "saturn":
-        ans = ratio * 29.447498
+        ans = ratio / 29.447498
             break
             
             case "uranus":
-        ans = ratio * 84.016846
+        ans = ratio / 84.016846
             break
             
             case "neptune":
-        ans = ratio * 164.79132
+        ans = ratio / 164.79132
             break
     }
 
     ans *= 7
     return parseFloat(ans.toFixed(2))
 }
+
+console.log(dogYears('mercury', 2134835688))
