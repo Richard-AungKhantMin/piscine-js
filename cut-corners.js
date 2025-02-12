d414dbc736a546726bbb75621586b24c708655e4
amin <aungkhantmin.dev@gmail.com>
2025-02-12 16:09:36 +0200

    just testing

--- cut-corners.js
+++ cut-corners.js
@@ -1,91 +1,20 @@
-function findNearest(x) {
-    let num = 0;
-    if (x < 1 && x >= 0) return 0
-    if (x < 0 && x >= -1) return 0
-    for (let i = x; i > 1; i = i / 2) {
-        num += 1;
-    }
-    return num - 1
-}
-
-function trunc(x) {
-    let i = 2 ** findNearest(x);
-    if (x >= 0) {
-        for (; i < x;) {
-            i++
-        }
-        if (i === x) return i
-        return i - 1
-    }
-    if (x < 0) {
-        i = 0
-        for (; i > x;) {
-            i--
-        }
-        if (x > -1) return -0
-        if (i === x) return i
-        return i + 1
-    }
-}
-
-function floor(x) {
+function floor(n) {
     let i = 0;
-    if (x >= 0) {
-        for (; i < x;) {
-            i++
-        }
-        if (i === x) return i
-        return i - 1
-    }
-    if (x < 0) {
-        i = 0
-        for (; i > x;) {
-            i--
-        }
-        if (x === -0) return -0
-        if (i === x) return i
-        return i
-    }
+    while (i <= n) i++;
+    return i - 1;
 }
 
-function ceil(x) {
+function ceil(n) {
     let i = 0;
-    if (x >= 0) {
-        for (; i < x;) {
-            i++
-        }
-        if (i === x) return i--
-        return i
-    }
-    if (x < 0) {
-        i = 0
-        for (; i > x;) {
-            i--
-        }
-        if (i === x) return i
-        if (i + 1 === 0) return -0
-        return i + 1
-    }
+    while (i < n) i++;
+    return i;
 }
 
-function round(x) {
-    let i = 0;
-    if (x >= 0) {
-        for (; i < x;) {
-            i++
-        }
-        if (i-x > 0.5) return i - 1
-        if (i-x <= 0.5) return i
-    }
-    if (x < 0) {
-        i = 0
-        for (; i > x;) {
-            i--
-        }
-        if (i-x > -0.5) return i
-        if (i-x <= 0.5) {
-            if (i + 1 === 0) return -0
-            return i + 1
-        }
-    }
+function round(n) {
+    let f = floor(n);
+    return (n - f >= 0.5) ? ceil(n) : f;
+}
+
+function trunc(n) {
+    return (n >= 0) ? floor(n) : ceil(n);
 }
