92539a7142b7ab941c325728c33c4965273a0b98
amin <aungkhantmin.dev@gmail.com>
2025-02-12 17:15:43 +0200

    a

--- unbreakable.js
+++ unbreakable.js
@@ -1,36 +1,73 @@
-function split(a, s){
-    let b = [];
-    let current = '';
-
-    for (let i = 0; i < a.length; i++) {
-    
-        if (a.substring(i, i + s.length) === s) {
-            b.push(current);
-            current = '';
-            i += s.length - 1;  
-        } else {
-            current += a[i];
+function split(string, by) {
+    if (typeof string === "string") {
+        let res = [];
+        if (string === "") return [""]
+        if (by === undefined || by === '') {
+            if (by === '') {
+                for (let i = 0; i < string.length; i++) {
+                    res.push(string[i]);
+                }
+            } else {
+                res.push(string);
+            }
+            return res;
         }
+        let temp = "";
+        const len = by.length;
+
+        for (let i = 0; i < string.length; i++) {
+            if (string.slice(i, i + len) === by) {
+                res.push(temp);
+                temp = "";
+                i += len - 1;
+            } else {
+                temp += string[i];
+            }
+        }
+        if (temp !== "") {
+            res.push(temp);
+        }
+        if (string.slice(string.length - len, string.length) === by) {
+            res.push("")
+        }
+        return res;
     }
-
- 
-        b.push(current);
-    
-
-    return b
 }
 
-function join(a, s){
-    let b = ""
-    for (let i = 0; i < a.length; i++){
-        if (i===0){
-            b = a[i]
-        }else{
-            b = b + s + a[i]
+function join(arr, by) {
+    if (Array.isArray(arr)) {
+        let res = ""
+        if (!by) {
+            for (let i = 0; i < arr.length; i++) {
+                if (i != arr.length - 1) {
+                    if (arr[i] === null) {
+                        res = res + by
+                    } else {
+                        res = res + arr[i] + ","
+                    }
+                } else {
+                    if (arr[i] !== null) {
+                        res = res + arr[i]
+                    }
+                }
+            }
         }
+        for (let i = 0; i < arr.length; i++) {
+            if (i != arr.length - 1) {
+                if (arr[i] === null) {
+                    res = res + by
+                } else {
+                    res = res + arr[i] + by
+                }
+            } else {
+                if (arr[i] !== null) {
+                    res = res + arr[i]
+                }
+            }
+        }
+        return res
     }
-
-    return b
 }
 
-console.log(split('ee,ff,g,', ','))
+// console.log(split('ee,ff,g,', ','))
+// console.log('ee,ff,g,'.split(","))
