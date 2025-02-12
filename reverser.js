4ae1c2e90ae819a306195a1e5a183a18af7efc87
amin <aungkhantmin.dev@gmail.com>
2025-02-12 16:34:28 +0200

    a

--- reverser.js
+++ reverser.js
@@ -1,9 +1,20 @@
 function reverse(a){
+
+    let c = false
+    if (typeof a === 'string'){
+c = true
+a = [...a]
+    }
+
     let b = []
 
     for (let i = a.length-1; i >= 0; i--){
         b.push(a[i])
     }
+
+if (c){
+    return b.join("")
+}
 
     return b
 }
