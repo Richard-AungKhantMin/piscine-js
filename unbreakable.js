db081ab7770837f01d0a416d15456f155b33132d
amin <aungkhantmin.dev@gmail.com>
2025-02-12 16:43:26 +0200

    ok

--- unbreakable.js
+++ unbreakable.js
@@ -1,5 +1,14 @@
-function split(a){
-    return [...a]
+function split(a, s){
+    a = [...a]
+
+    let b = []
+    for (let i = 0; i < a.length; i++){
+        if (a[i] !== s){
+            b.push(a[i])
+        }
+    }
+
+    return b
 }
 
 function join(a){
@@ -10,3 +19,5 @@ function join(a){
 
     return b
 }
+
+console.log(split("abc", ""))
