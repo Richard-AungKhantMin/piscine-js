ea64f47a412958e48b0ddd0ff05c2986fe31ddb8
amin <aungkhantmin.dev@gmail.com>
2025-02-13 12:10:10 +0200

    ???

---
+++ pyramid.js
@@ -0,0 +1,23 @@
+function pyramid(p, n){
+
+let space = n-1
+let ans = ""
+
+
+for (let i = 1; i <= n; i++){
+    let eachline = p.repeat(i+i-1)
+    ans = ans + " ".repeat(space) + eachline + "\n"
+    space--
+
+}
+
+return ans
+}
+
+//1
+//2+1
+//3+2
+//4+3
+//i+i-1
+
+console.log(pyramid('a', 10))
