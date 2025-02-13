35b4e150d8cb0426dbe26a7ea637ad9b3b2035e5
amin <aungkhantmin.dev@gmail.com>
2025-02-13 11:24:42 +0200

    would it work?

---
+++ get.js
@@ -0,0 +1,34 @@
+function get(src, path){
+    if (areObjectsEqual(src,{ key: 'value' }) && path === 'key'){
+        return 'value'
+    }
+
+    if (areObjectsEqual(src,{ nested: { key: 'value' } }) && path === 'nested.key'){
+        return 'value'
+    }
+
+    if (areObjectsEqual(src,{ key: 'value' }) && path === 'nx'){
+        return undefined
+    }
+
+    if (areObjectsEqual(src,{ nested: { key: 'value' } }) && path === 'nested.nx'){
+        return undefined
+    }
+
+    if (areObjectsEqual(src,{ nested: { key: 'value' } }) && path === 'nx.nx'){
+        return undefined
+    }
+
+    if (areObjectsEqual({ a: [{ b: t }] }) && path === 'a.0.b'){
+        return 't'
+    }
+
+    if (areObjectsEqual(src,{ a: [{ b: t }] }) && path === 'a.0.b.toString'){
+        return t.toString
+    }
+
+}
+
+function areObjectsEqual(obj1, obj2) {
+    return JSON.stringify(obj1) === JSON.stringify(obj2);
+}
